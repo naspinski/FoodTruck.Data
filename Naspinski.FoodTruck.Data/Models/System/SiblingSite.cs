7dfd1d1ee7e7004f3d7abb74251074961f4c383c
naspinski <stan@naspinski.net>
2020-03-29 10:58:25 -0500

    test

--- Naspinski.FoodTruck.Data/Models/System/SiblingSite.cs
+++ Naspinski.FoodTruck.Data/Models/System/SiblingSite.cs
@@ -5,8 +5,9 @@ namespace Naspinski.FoodTruck.Data.Models.System
 {
     public class SiblingSite : AuditCreateUpdateDeleteBase
     {
+        //wtf
         [Key]
-        public int Id { get; set; }
+        public int Id { get; set; } 
 
         [Required]
         public string Name { get; set; }
