46d3ed6baac9cbf223a1e5ab1070e1a68888cba1
naspinski <stan@naspinski.net>
2020-03-29 10:47:39 -0500

    added migration

---
+++ Naspinski.FoodTruck.Data/Migrations/20200329154655_addSiblings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Naspinski.FoodTruck.Data.Migrations
+{
+    public partial class addSiblings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SiblingSites",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Updated = table.Column<DateTimeOffset>(nullable: true),
+                    UpdatedBy = table.Column<string>(nullable: true),
+                    Created = table.Column<DateTimeOffset>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: false),
+                    Deleted = table.Column<DateTimeOffset>(nullable: true),
+                    DeletedBy = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: false),
+                    Url = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SiblingSites", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SiblingSites");
+        }
+    }
+}
