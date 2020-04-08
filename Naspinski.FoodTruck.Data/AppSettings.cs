ddfa7cca22f8311fa1fb329c8c52057b9e9c4ef0
naspinski <stan@naspinski.net>
2020-04-08 13:08:23 -0500

    cleaning up configs

--- Naspinski.FoodTruck.Data/AppSettings.cs
+++ Naspinski.FoodTruck.Data/AppSettings.cs
@@ -15,20 +15,11 @@ namespace Naspinski.FoodTruck.Data
         public string SendgridApiKey { get; set; }
         public string AdminSiteBaseUrl { get; set; }
         public string SiteBaseUrl { get; set; }
-        public string HomeUrl { get; set; }
         public string CorsAddresses { get; set; }
     }
 
     public class SquareSettings
     {
-        public bool UseProductionApi { get; set; }
-        public string ProductionApplicationId { get; set; }
-        public string ProductionAccessToken { get; set; }
-        public string ProductionLocationId { get; set; }
-        public string SandboxApplicationId { get; set; }
-        public string SandboxAccessToken { get; set; }
-        public string SandboxLocationId { get; set; }
-
         public IEnumerable<SquareLocation> Locations { get; set; }
     }
 
