7b968a0daa8041bb2429ab6d217d1279481bbd9a
Stan Naspinski <stan@naspinski.net>
2020-06-09 16:23:06 -0500

    added a get by square order id

---
+++ Naspinski.FoodTruck.Data.Access/Queries/Orders/GetBySquareId.cs
@@ -0,0 +1,22 @@
+﻿using Naspinski.Data.CommandQuery.Base;
+using Naspinski.FoodTruck.Data.Models.Payment;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Naspinski.FoodTruck.Data.Access.Queries.Orders
+{
+    public class GetBySquareId : ResultQueryBase<FoodTruckContext, Order>
+    {
+        private readonly string _squareOrderId;
+
+        public GetBySquareId(FoodTruckContext context, string squareOrderId) : base(context)
+        {
+            _squareOrderId = squareOrderId;
+        }
+
+        protected override Order InternalExecute()
+        {
+            return _context.Orders.Include(x => x.Items).SingleOrDefault(x => x.SquareOrderId.Equals(_squareOrderId, System.StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
