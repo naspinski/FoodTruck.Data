42693f3686dcf8a3ed0e7409b997e3feaae6b142
Stan Naspinski <stan@naspinski.net>
2023-03-01 19:53:10 -0600

    Update Document.cs

--- Naspinski.FoodTruck.Data/Models/Storage/Document.cs
+++ Naspinski.FoodTruck.Data/Models/Storage/Document.cs
@@ -1,6 +1,5 @@
 ﻿using Naspinski.Data.Model;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.IO;
@@ -13,6 +12,8 @@ namespace Naspinski.FoodTruck.Data.Models.Storage
         public static class DocType
         {
             public const string Pdf = "application/pdf";
+            public const string Jpeg = "image/jpeg";
+            public const string Png = "image/png";
 
             public static string FromFileName(string filename)
             {
@@ -20,6 +21,9 @@ namespace Naspinski.FoodTruck.Data.Models.Storage
                 switch(ext)
                 {
                     case ".pdf": return Pdf;
+                    case ".png": return Png;
+                    case ".jpg": case ".jpeg": return Jpeg;
+
                     default:
                         throw new NotImplementedException($"extension: {ext} has not been implemented yet");
                 }
